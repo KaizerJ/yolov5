2769b27fe67ec5013f84c6a0b2f46c7f77a2b588
Jonay Suarez <jonay.suarez104@alu.ulpgc.es>
2022-03-04 14:30:06 +0000

    Print detect output shape

--- models/yolo.py
+++ models/yolo.py
@@ -150,6 +150,7 @@ class Model(nn.Module):
             y.append(x if m.i in self.save else None)  # save output
             if visualize:
                 feature_visualization(x, m.type, m.i, save_dir=visualize)
+        print(x.shape)
         return x
 
     def _descale_pred(self, p, flips, scale, img_size):
